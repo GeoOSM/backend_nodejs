8bdc7c761adb0c5843674d254d7a81794b95a096
TANK <nelsontayou1@gmail.com>
2019-11-10 13:18:44 +0100

    update

--- server.js
+++ server.js
@@ -1119,7 +1119,8 @@ app.post('/analyse_spatiale', cors(corsOptions), function (req, res) {
 					}
 				})
 			} else if (methode == 'qgis') {
-				var projet_qgis = path_projet_qgis + donne['querry'][k]['projet_qgis'] + ".qgs"
+				var destination = pte_projet(donne['querry'][k]['projet_qgis']).destination
+				var projet_qgis = destination + '/../' + donne['querry'][k]['projet_qgis'] + '.qgs'
 				var layername = donne['querry'][k]['identifiant']
 				let options = {
 					mode: 'text',
