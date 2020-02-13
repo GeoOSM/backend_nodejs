f886d41d6eb2475c8751c84349f311a3c4802491
TANK <nelsontayou1@gmail.com>
2020-02-13 16:13:01 +0100

    add: fichier pour faire le backup des projets QGIS (à termier)

---
+++ backup_project_geosm.py
@@ -0,0 +1,22 @@
+import os
+import glob
+from os.path import join, basename, exists, dirname
+import os, shutil, typing, csv
+from pathlib import Path
+from datetime import date,datetime
+
+
+backup_dir = "/var/www/geosm/mali/backup/"
+projet = "mali"
+file_projet = "/var/www/geosm/mali/"
+
+now = datetime.now()
+today = date.today().strftime("%d%m%Y")
+time = now.strftime("%H%M%S")
+
+backup_dir_of_today = backup_dir+today
+Path(backup_dir_of_today).mkdir(parents=True, exist_ok=True)
+
+item_path = file_projet+projet+'.qgs'
+to_dir = backup_dir_of_today+'/'+projet+'_'+time+'.qgs'
+shutil.copy(item_path, to_dir)
