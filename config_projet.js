f8ed2c6abe2c69ab431bfd94860eb33cae7acef1
TANK <nelsontayou1@gmail.com>
2020-03-17 19:13:45 +0100

    update : - mise à jour de la config d'un projet pour la bd, ajout de user,password et port - mise à jour du backup de la bd

--- config_projet.js
+++ config_projet.js
@@ -3,11 +3,11 @@ const fs = require('fs');
 let list_projet = JSON.parse(fs.readFileSync('projet.json'))['projet'];
 
 const bd_access = {
-	user: 'postgres',
+	user: '',
 	host: 'localhost',
 	database: '',
-	password: 'postgres',
-	port: 5432,
+	password: '',
+	port: '',
 }
 
 let get_projet_pt = function(projet){
@@ -15,10 +15,16 @@ let get_projet_pt = function(projet){
 		var destination =list_projet[projet].destination
 		var destination_style =list_projet[projet].destination_style
 		var database =list_projet[projet].database
+		var password =list_projet[projet].password
+		var port =list_projet[projet].port
+		var user =list_projet[projet].user
 		var path_backend = list_projet[projet].path_backend
 	}
     
     bd_access.database = database
+    bd_access.password = password
+    bd_access.port = port
+    bd_access.user = user
     
     return {
         bd_access:bd_access,
